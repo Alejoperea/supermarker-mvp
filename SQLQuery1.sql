2e04e9bd739af0789e911d4bcc2921545127878c
Milton Alejandro Perea <mpereamejia@gmail.com>
2023-10-21 18:35:11 -0500

    Initial Project Providers

---
+++ SQLQuery1.sql
@@ -0,0 +1,10 @@
+﻿use Supermarker
+create table Products
+(
+ Products_Id int identity (100000,1) primary key,
+ Products_Name nvarchar (50) not null,
+ Products_Observation nvarchar (50) not null,
+)
+go
+insert into Products values('Yogourt', 'Lacteos')
+insert into Products values('Credit card', 'Credit card mode')
