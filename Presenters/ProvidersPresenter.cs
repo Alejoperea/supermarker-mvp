80ca468e28c8f87e0508046dec7cefa563cfb03a
Milton Alejandro Perea <mpereamejia@gmail.com>
2023-10-21 20:33:45 -0500

    Providers Presenter

---
+++ Presenters/ProvidersPresenter.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Supermarket_mvp.Views;
+using Supermarket_mvp.Models;
+
+namespace Supermarket_mvp.Presenters
+{
+    internal class ProvidersPresenter
+    {
+        private IProvidersView view;
+        private IProvidersRepository repository;
+        private BindingSource providersBindingSource;
+        private IEnumerable<ProvidersModel> providersModeList;
+
+        public ProvidersPresenter(IProvidersView view, IProvidersRepository repository)
+        {
+            this.providersBindingSource = new BindingSource();
+
+            this.view = view;
+            this.repository = repository;
+
+            this.view.SearchEvent += SearchProviders;
+            this.view.AddNewEvent += AddNewProviders;
+            this.view.EditEvent += LoadSelectProvidersToEdit;
+            this.view.DeleteEvent += DeleteSelectedProviders;
+            this.view.SaveEvent += SaveProviders;
+            this.view.CancelEvent += CancelAction;
+
+            this.view.SetProvidersListBildingSource(providersBindingSource);
+
+            loadAllProvidersList();
+
+            this.view.Show();
+        }
+
+        private void loadAllProvidersList()
+        {
+            providersModeList = repository.GetAll();
+            providersBindingSource.DataSource = providersModeList;
+        }
+
+        private void SearchProviders(object? sender, EventArgs e)
+        {
+            bool emptyValue = string.IsNullOrWhiteSpace(this.view.SearchValue);
+            if (emptyValue == false)
+            {
+                providersModeList = repository.GetByValue(this.view.SearchValue);
+            }
+            else
+            {
+                providersModeList = repository.GetAll();
+            }
+            providersBindingSource.DataSource = providersModeList;
+        }
+
+        private void AddNewProviders(object? sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void LoadSelectProvidersToEdit(object? sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void DeleteSelectedProviders(object? sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void SaveProviders(object? sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void CancelAction(object? sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
