02e550093d31cd3fa74b433de2801efa9fb81f7c
Milton Alejandro Perea <mpereamejia@gmail.com>
2023-10-22 05:10:38 -0500

    Categories Presenter

---
+++ Presenters/CategoriesPresenter.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Supermarket_mvp.Views;
+using Supermarket_mvp.Models;
+
+namespace Supermarket_mvp.Presenters
+{
+    internal class CategoriesPresenter
+    {
+        private ICategoriesView view;
+        private ICategoriesRepository repository;
+        private BindingSource categoriesBindingSource;
+        private IEnumerable<CategoriesModel> categoriesList;
+
+        public CategoriesPresenter(ICategoriesView view, ICategoriesRepository repository)
+        {
+            this.categoriesBindingSource = new BindingSource();
+
+            this.view = view;
+            this.repository = repository;
+
+            this.view.SearchEvent += SearchCategories;
+            this.view.AddNewEvent += AddNewCategories;
+            this.view.EditEvent += LoadSelectCategoriesToEdit;
+            this.view.DeleteEvent += DeleteSelectedCategories;
+            this.view.SaveEvent += SaveCategories;
+            this.view.CancelEvent += CancelAction;
+
+            this.view.SetCategoriesListBildingSource(categoriesBindingSource);
+
+            loadAllCategoriesList();
+
+            this.view.Show();
+        }
+
+        private void loadAllCategoriesList()
+        {
+            categoriesList = repository.GetAll();
+            categoriesBindingSource.DataSource = categoriesList;
+        }
+
+        private void SearchCategories(object? sender, EventArgs e)
+        {
+            bool emptyValue = string.IsNullOrWhiteSpace(this.view.SearchValue);
+            if (emptyValue == false)
+            {
+                categoriesList = repository.GetByValue(this.view.SearchValue);
+            }
+            else
+            {
+                categoriesList = repository.GetAll();
+            }
+            categoriesBindingSource.DataSource = categoriesList;
+        }
+
+        private void AddNewCategories(object? sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void LoadSelectCategoriesToEdit(object? sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void DeleteSelectedCategories(object? sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void SaveCategories(object? sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void CancelAction(object? sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
